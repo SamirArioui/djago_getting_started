68ad9e4533d914f1c7860f3e6515a554bf494fed
Samir Arioui <arioui.samir@gmail.com>
2022-12-02 21:37:55 +0100

    add models

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,5 +1,15 @@
+from datetime import datetime
+
 from django.http import HttpResponse
 
 
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Planner!")
+
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+
+def about(request):
+    return HttpResponse("My name is Samir Arioui")
